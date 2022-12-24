c08bb0f493de28245e3bb573e925b159c114a973
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2022-12-24 17:36:20 +0200

    Enabled CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const config = require('config');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRouter = require('./router/UserRoute');
 const productRouter = require('./router/ProductRoute');
 const {handleError} = require('./exceptions/ExceptionHandler')
@@ -16,6 +17,7 @@ if(!config.has('jwtPrivateKey')){
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use(handleError);
@@ -28,5 +30,5 @@ console.log(`Application name: ${config.get('name')}`);
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
 app.listen(port, () => {
-    console.log(`Server started on host: ${host} port ${port}..`)
+    console.log(`Server started on Host: ${host} and Port: ${port}..`)
 });
