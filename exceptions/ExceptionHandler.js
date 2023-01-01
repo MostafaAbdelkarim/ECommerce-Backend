2cff175248bce77e7be3ca6e0133329895f45976
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2023-01-01 15:29:04 +0200

    Trying console.error in production

--- exceptions/ExceptionHandler.js
+++ exceptions/ExceptionHandler.js
@@ -3,6 +3,7 @@ const pino = require('../config/Logger');
 const handleError = (err,req,res,next) => {
   pino.error(`${req.method} ${req.protocol}://${req.hostname}${req.originalUrl} - IP: ${req.ip} - HOST: ${req.get('Host')} - AGENT: ${req.get('User-Agent')}`)
   pino.error(`STATUS: ${err.status} - STATUSCODE: ${err.statusCode} - MESSAGE: ${err.message} \n`)
+  console.error(`${req.method} ${req.protocol}://${req.hostname}${req.originalUrl} - IP: ${req.ip} - HOST: ${req.get('Host')} - AGENT: ${req.get('User-Agent')}`)
   res.status(err.statusCode || 500).send({ 
     timestamp: new Date(), 
     statusCode: err.statusCode || 500, 
