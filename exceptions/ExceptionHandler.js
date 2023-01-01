e6f25f70a04fb415ec1dc6f279e18362fbc207ec
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2023-01-01 20:16:22 +0200

    added dash

--- exceptions/ExceptionHandler.js
+++ exceptions/ExceptionHandler.js
@@ -2,8 +2,8 @@ const logger = require('../config/Logger');
 const {currentTimeStamp} = require('../config/TimeStamp');
 
 const handleError = (err,req,res,next) => {
-  const userIp = req.socket.remoteAddress || req.headers['x-forwarded-for'] || req.ip;
-  logger.error(`${req.method} ${req.originalUrl} - IP: ${userIp} - AGENT: ${req.get('User-Agent')}`+` STATUS: ${err.status} - STATUSCODE: ${err.statusCode} - MESSAGE: ${err.message}`);
+  const userIp = req.socket.remoteAddress || req.headers['X-Forwarded-For'] || req.ip;
+  logger.error(`${req.method} ${req.originalUrl} - IP: ${userIp} - AGENT: ${req.get('User-Agent')} - STATUS: ${err.status} - STATUSCODE: ${err.statusCode} - MESSAGE: ${err.message}`);
   res.status(err.statusCode || 500).send({ 
     timestamp: currentTimeStamp, 
     statusCode: err.statusCode || 500, 
