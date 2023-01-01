22edd4a46d459c6669669c3c3f44700087cfe331
Mostafa ElDahshan <mostafaabdelkarim22@gmail.com>
2023-01-01 17:10:21 +0200

    Modifying logging messages

--- exceptions/ExceptionHandler.js
+++ exceptions/ExceptionHandler.js
@@ -2,8 +2,8 @@ const pino = require('../config/Logger');
 const {currentTimeStamp} = require('../config/TimeStamp');
 
 const handleError = (err,req,res,next) => {
-  pino.error(`${currentTimeStamp} ${req.method} ${req.protocol}://${req.hostname}${req.originalUrl} - IP: ${req.ips} - AGENT: ${req.get('User-Agent')}`)
-  pino.error(`STATUS: ${err.status} - STATUSCODE: ${err.statusCode} - MESSAGE: ${err.message}`)
+  pino.error(`${currentTimeStamp} ${req.method} ${req.protocol}://${req.hostname}${req.originalUrl} - IP: ${req.ip} - AGENT: ${req.get('User-Agent')}`)
+  pino.error(`${currentTimeStamp} STATUS: ${err.status} - STATUSCODE: ${err.statusCode} - MESSAGE: ${err.message}`)
   res.status(err.statusCode || 500).send({ 
     timestamp: currentTimeStamp, 
     statusCode: err.statusCode || 500, 
